1926667ee303ccdd5efcf8d2d0ec51f1c5a86932
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-09-17 18:20:40 -0300

    feat: global styles and primary button

---
+++ components/ButtonPrimary.js
@@ -0,0 +1,18 @@
+import styles from '../styles/ButtonPrimary.module.scss';
+
+const ButtonPrimary = ({ children }) => {
+  return (
+    <button className={styles['btn-primary']}>
+      <div className={styles['btn-left-box']}>
+        <div className={styles['arrows-container']}>
+          <div className={styles['stroke-one']}></div>
+          <div className={styles['stroke-two']}></div>
+          <div className={styles['stroke-three']}></div>
+        </div>
+      </div>
+      {children}
+    </button>
+  );
+};
+
+export default ButtonPrimary;
