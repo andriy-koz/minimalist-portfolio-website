c4f791f3d5dde8aa9fc0b6622f1f12648bb13059
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-09-17 18:36:21 -0300

    working on secondary button

---
+++ components/ButtonSecondary.js
@@ -0,0 +1,7 @@
+import styles from '../styles/ButtonSecondary.module.scss';
+
+const ButtonSecondary = ({ children }) => {
+  return <button className={styles['btn-secondary']}>{children}</button>;
+};
+
+export default ButtonSecondary;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import ButtonPrimary from '../components/ButtonPrimary';
+import ButtonSecondary from '../components/ButtonSecondary';
 
 export default function Home() {
   return (
     <div>
       <ButtonPrimary>about me</ButtonPrimary>
+      <ButtonSecondary>secondary</ButtonSecondary>
     </div>
   );
 }
