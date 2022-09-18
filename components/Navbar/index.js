029416dd5d7458a7b34b06b371555348c22a2ace
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-09-18 16:25:09 -0300

    finishing the navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,6 +4,12 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav className={`container ${styles.navbar}`}>
+      <div className={styles.logo}>
+        <div className={styles['logo-triangle']} />
+        <div
+          className={`${styles['logo-triangle']} ${styles['logo-triangle-2']}`}
+        />
+      </div>
       <div className={styles.links}>
         <Link href=''>home</Link>
         <Link href=''>portfolio</Link>
