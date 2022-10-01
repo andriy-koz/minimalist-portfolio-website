e6a7a429ff3698fb7f4dff135b202254019a7726
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-10-01 09:21:02 -0300

    Advancing with Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,16 @@
+import Link from 'next/link';
 import styles from '../styles/Footer.module.scss';
+import TrianglesLogo from './UI/TrianglesLogo';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={`${styles['footer-container']} container`}></div>
+      <div className={`${styles['footer-container']} container`}>
+        <TrianglesLogo footer />
+        <Link href=''>home</Link>
+        <Link href=''>portfolio</Link>
+        <Link href=''>contact me</Link>
+      </div>
     </div>
   );
 };
--- components/UI/TrianglesLogo.js
+++ components/UI/TrianglesLogo.js
@@ -1,11 +1,17 @@
 import styles from '../../styles/TrianglesLogo.module.scss';
 
-const TrianglesLogo = () => {
+const TrianglesLogo = ({ footer }) => {
   return (
     <div className={styles.logo}>
-      <div className={styles['logo-triangle']} />
       <div
-        className={`${styles['logo-triangle']} ${styles['logo-triangle-2']}`}
+        className={`${styles['logo-triangle']} ${
+          footer && styles['logo-footer']
+        }`}
+      />
+      <div
+        className={`${styles['logo-triangle']} ${styles['logo-triangle-2']} ${
+          footer && styles['logo-footer']
+        }`}
       />
     </div>
   );
