6c434d80e3a56858a6ad3dde9091c0849ffe8437
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-11-06 20:20:53 -0300

    feat: added hamburger menu to nav-bar

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className={styles['main-container']}>
       <div className="container">
         <div className={styles.info}>
-          <h1>Hey, I&apos;m Andriy and I love building beautiful websites</h1>
+          <h1>Hey, I&apos;m Alex Spencer and I love building beautiful websites</h1>
           <a href="#about-me">
             <ButtonPrimary>about me</ButtonPrimary>
           </a>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import LogoTriangles from './UI/LogoTriangles'
 import styles from '../styles/components/Navbar.module.scss'
 
 const Navbar = () => {
   const router = useRouter()
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const menuHandler = () => {
+    setOpenMenu(prevState => !prevState);
+  }
 
   return (
     <nav className={`container ${styles.navbar}`}>
@@ -24,6 +31,28 @@ const Navbar = () => {
           <Link href="/contact">contact me</Link>
         </li>
       </ul>
+
+      {/* MOBILE */}
+
+      <div className={styles.menu}>
+        <Image onClick={menuHandler} src={'/icons/hamburger.svg'} width={24} height={13} alt='Hamburger icon'/>
+        <ul className={`${styles['mobile-menu']} ${!openMenu ? styles.hide : ''}`}> 
+        <li className={router.pathname === '/' ? styles.active : ''} onClick={menuHandler}>
+          <Link href="/">home</Link>
+        </li>
+        <li
+        onClick={menuHandler}
+          className={
+            router.pathname.match(/\/portfolio.*/gm) ? styles.active : ''
+          }
+        >
+          <Link href="/portfolio">portfolio</Link>
+        </li>
+        <li className={router.pathname === '/contact' ? styles.active : ''} onClick={menuHandler}>
+          <Link href="/contact">contact me</Link>
+        </li>
+        </ul>
+      </div>
     </nav>
   )
 }
